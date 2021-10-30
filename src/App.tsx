1ec1160f17898caa968ac8af4c4897ea415d0cc1
CodingDaria <dasha.fbi@gmail.com>
2021-10-30 14:34:51 +0300

    add tailwind eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="w-8 h-8" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p className="text-red-600 font-bold">TEST</p>
         <a
           className="App-link"
           href="https://reactjs.org"
