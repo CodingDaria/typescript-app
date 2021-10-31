7c2a960c2ed9ee843c0d2dfb321e477437ec98db
CodingDaria <dasha.fbi@gmail.com>
2021-10-31 21:20:08 +0200

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './assets/css/index.css';
 
+import reportWebVitals from './reportWebVitals';
+import { getStore } from './store';
+
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
+const { store, persistor } = getStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
