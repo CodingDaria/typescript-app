1eca3b1254b4c4c7a4cce4db5b1a03b181743187
CodingDaria <dasha.fbi@gmail.com>
2021-10-31 21:33:07 +0200

    add router

---
+++ src/components/Test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+
+const Test: () => JSX.Element = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="w-8 h-8" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p className="text-red-600 font-bold">TEST</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Test;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import './assets/css/index.css';
 
@@ -16,7 +17,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
