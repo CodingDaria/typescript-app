2cf74c67013d1d55c4cd0a44c0101e2bab3d1524
CodingDaria <dasha.fbi@gmail.com>
2021-10-31 21:33:07 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Test from './components/Test';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="w-8 h-8" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p className="text-red-600 font-bold">TEST</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/test" />
+      </Route>
+      <Route exact path="/test" component={Test} />
+    </Switch>
   );
 }
 
