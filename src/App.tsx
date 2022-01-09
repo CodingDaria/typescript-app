1dfb35120656767bfdd682d987e8ef2c13dac1fc
CodingDaria <dasha.fbi@gmail.com>
2022-01-09 13:27:18 +0200

    app: migrate to react-router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Test from './components/Test';
 
 function App(): JSX.Element {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/test" />
-      </Route>
-      <Route exact path="/test" component={Test} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<Navigate to="/test" />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
   );
 }
 
